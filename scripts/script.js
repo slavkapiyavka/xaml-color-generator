70731865049688543599f00487b80e31942f5adb
Vyacheslav Nikolaev <46050023+slavkapiyavka@users.noreply.github.com>
2023-06-13 18:26:43 +0200

    add possibility to remove color

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,6 @@
 let colorNameInputElements = document.querySelectorAll('.color-name-input');
 let colorCodeInputElements = document.querySelectorAll('.color-code-input');
+let removeColorButtons = document.querySelectorAll('.remove-color-button');
 const GENERATION_RESULT_TEXTAREA_ELEMENT = document.querySelector('.generation-result-textarea');
 const GENEREATE_BUTTON_ELEMENT = document.querySelector('.generate-button');
 const ADD_COLOR_BUTTON_ELEMENT = document.querySelector('.add-color-button');
@@ -25,9 +26,22 @@ const addColorTemplate = (evt) => {
     const colorElement = FORM_ROW_GROUP_TEMPLATE.cloneNode(true);
     FORM_ROW_GROUP_FRAGMENT.appendChild(colorElement);
     COLORS_CONTAINER.appendChild(FORM_ROW_GROUP_FRAGMENT);
-    colorNameInputElements = document.querySelectorAll('.color-name-input');
-    colorCodeInputElements = document.querySelectorAll('.color-code-input');
+    updateElements();
 }
 
+function removeColor (evt) {
+    evt.preventDefault();
+    COLORS_CONTAINER.removeChild(this.parentElement);
+    updateElements();
+}
+
+const updateElements = () => {
+    colorNameInputElements = document.querySelectorAll('.color-name-input');
+    colorCodeInputElements = document.querySelectorAll('.color-code-input');
+    removeColorButtons = document.querySelectorAll('.remove-color-button');
+    removeColorButtons.forEach(button => button.addEventListener('click', removeColor));
+}
+
+removeColorButtons.forEach(button => button.addEventListener('click', removeColor));
 GENEREATE_BUTTON_ELEMENT.addEventListener('click', generateResult);
 ADD_COLOR_BUTTON_ELEMENT.addEventListener('click', addColorTemplate);
